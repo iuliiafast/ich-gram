860ae01f4aa776f96a63f0c9d56a9c38a19a54e5
Iuliia_Fast <iuliia914@gmail.com>
2024-11-19 17:09:35 +0100

    Changes to be committed:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClientProviders>
-          <TokenInitializer /> {/* Теперь токен проверяется */}
+          <TokenInitializer />
           {children}
         </ClientProviders>
       </body>
